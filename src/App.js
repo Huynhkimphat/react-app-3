1e07f638ed99efadc1e2e8d8500c88ae013d15ac
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-05-26 22:41:17 +0700

    Finsh Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-
+import React, { useState } from "react";
+import UsersList from "./components/Users/UsersList";
 import AddUser from "./components/Users/AddUser";
 function App() {
+    const [usersList, setUsersList] = useState([]);
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevState) => {
+            return [
+                ...prevState,
+                { name: userName, age: userAge, id: Math.random().toString() },
+            ];
+        });
+    };
     return (
         <div>
-            <AddUser />
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
         </div>
     );
 }
